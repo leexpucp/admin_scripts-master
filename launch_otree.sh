ec6242e3d3bdec049461fa51e53fbf2b2be86f8e
Carlo1911 <carlo.alva@pucp.pe>
2017-08-18 11:56:36 -0500

    fullscreen e incognito

--- launch_otree.sh
+++ launch_otree.sh
@@ -10,5 +10,5 @@ ADDRESS="192.168.46.20:8000"
 echo "opening otree at $ADDRESS on machines $MACHINES"
 
 ./run_on_machines.sh "ID=\`hostname | sed 's/leex-//g'\`; \
-					 DISPLAY=:0 google-chrome --kiosk --disable-background-timer-throttling $ADDRESS/room/EconoLab/?participant_label=leex_\$ID 2>/dev/null" \
+					 DISPLAY=:0 google-chrome --kiosk --start-maximized --disable-background-timer-throttling --incognito $ADDRESS/room/EconoLab/?participant_label=\$ID 2>/dev/null" \
 					 $MACHINES &
